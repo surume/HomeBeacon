ed11056fcc3dafa79e34c7ea2750ceb0b8f8d320
Surume <mail@sururne.com>
2015-07-02 21:52:00 +0900

    Initial Commit

---
+++ HomeBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeBeacon
+//
+//  Created by miyamotoshota on 2015/07/02.
+//  Copyright (c) 2015年 sururne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
